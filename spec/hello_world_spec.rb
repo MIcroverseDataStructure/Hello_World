e1ddaa456a5e90049e223e0329ede48e288d9366
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-02-22 23:28:51 +0500

    Add spec for helloworld file

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,13 +1,13 @@
-# spec/game_logic_test_spec.rbc
+# spec/hello_world_spec.rbc
 
 require_relative '../lib/hello_world'
 
 describe HelloWorld do
   describe '#printhello' do
-    it 'must return hello world' do
-      p HelloWorld.printhello()
-      # puts result
-      # expect(result).to equel('Hello, World!')
+  it 'must return hello world' do
+      hello = HelloWorld.new
+      result = hello.printhello()
+      expect(result).to eql("Hello, World!")
     end
   end
 end
